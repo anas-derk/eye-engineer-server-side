341eaad42e308cff80ca8e0907f1089129e418ea
anas derk <anas.derk@gmail.com>
2025-07-27 15:28:46 +0100

    Test

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -66,6 +66,7 @@ async function postLoginWithGoogle(req, res) {
         });
     }
     catch (err) {
+        console.log(err);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
